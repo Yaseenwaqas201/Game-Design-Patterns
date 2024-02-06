186a1d020c8867feecd18dd5f529c40f7aaed0ef
Yaseenwaqas201 <59902640+Yaseenwaqas201@users.noreply.github.com>
2024-02-06 11:06:25 +0500

    Changes

---
+++ Assets/_Project/Command Pattern/Scripts/UI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CommandPattern{
+    public class UI : MonoBehaviour{
+        [SerializeField] private Button _undoButton;
+        [SerializeField] private Button _redoButton;
+        [SerializeField] private GridMovement _gridMovement;
+        [SerializeField] private Character _character;
+
+        private void Awake(){
+            _character.OnWalkStateChanged += isWalking => {
+                if (isWalking){
+                    _undoButton.interactable = false;
+                    _redoButton.interactable = false;
+                }
+                else{
+                    _undoButton.interactable = _gridMovement.CanUndo;
+                    _redoButton.interactable = _gridMovement.CanRedo;
+                }
+            };
+        }
+    }
+}
